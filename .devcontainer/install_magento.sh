750f9e86759bf1d7b76b1239a136ba90d787c06e
Jason Evans <evans022@gmail.com>
2023-02-15 21:26:52 +0000

    Initial commit of magento setup shell scripts.

---
+++ .devcontainer/install_magento.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+set -eu
+
+url=$(gp url | awk -F"//" {'print $2'}) && url+="/" &&
+url="https://8080-"$url"/"
+
+INSTALL_MAGENTO="${INSTALL_MAGENTO:=YES}"
+INSTALL_SAMPLE_DATA="${INSTALL_SAMPLE_DATA:=YES}"
+MAGENTO_EDITION="${MAGENTO_EDITION:=community}"
+MAGENTO_ADMIN_USERNAME="${MAGENTO_ADMIN_USERNAME:=admin}"
+MAGENTO_ADMIN_PASSWORD="${MAGENTO_ADMIN_PASSWORD:=password1}"
+INSTALL_HYVA="${INSTALL_HYVA:=NO}"
+
+cd ${CODESPACES_REPO_ROOT}
+
+if [ "${INSTALL_MAGENTO}" = "YES" ]; then
+  ddev redis-cli flushall
+  ddev exec rm -rf /var/www/html/var/cache/ /var/www/html/generated/
+  cp .gitpod/config.php app/etc/config.php
+  rm -rf app/etc/env.php
+  ddev magento setup:install --db-name='db' --db-user='db' --db-password='db' --base-url=$url --backend-frontname='admin' --admin-user=$MAGENTO_ADMIN_USERNAME --admin-password=$MAGENTO_ADMIN_PASSWORD --admin-email=$GITPOD_GIT_USER_EMAIL --admin-firstname='Admin' --admin-lastname='User' --use-rewrites='1' --use-secure='1' --base-url-secure=$url --use-secure-admin='1' --language='en_US' --db-host='db' --cleanup-database --timezone='America/Chicago' --currency='USD' --session-save='files' --search-engine='elasticsearch7' --elasticsearch-host='elasticsearch' --elasticsearch-port='9200' || true
+  #Run setup:install again because of error when installing Magento from scratch.
+  ddev magento setup:install --db-name='db' --db-user='db' --db-password='db' --base-url=$url --backend-frontname='admin' --admin-user=$MAGENTO_ADMIN_USERNAME --admin-password=$MAGENTO_ADMIN_PASSWORD --admin-email=$GITPOD_GIT_USER_EMAIL --admin-firstname='Admin' --admin-lastname='User' --use-rewrites='1' --use-secure='1' --base-url-secure=$url --use-secure-admin='1' --language='en_US' --db-host='db' --cleanup-database --timezone='America/Chicago' --currency='USD' --session-save='files' --search-engine='elasticsearch7' --elasticsearch-host='elasticsearch' --elasticsearch-port='9200';
+fi
+
+if [ "${INSTALL_SAMPLE_DATA}" = "YES" ]; then
+  ddev magento sampledata:deploy;
+fi
+
+if [ "${MAGENTO_EDITION}" = "enterprise" ]; then
+    ddev composer require magento/extension-b2b;
+fi
+
+if [ "${INSTALL_HYVA}" = "YES" ] && [ ! -z "${HYVA_COMPOSER_TOKEN}" ] && [ ! -z "${HYVA_COMPOSER_PROJECT}" ]; then
+    ddev composer config --auth http-basic.hyva-themes.repo.packagist.com token ${HYVA_COMPOSER_TOKEN}
+    ddev composer config repositories.private-packagist composer https://hyva-themes.repo.packagist.com/${HYVA_COMPOSER_PROJECT}/
+    ddev composer require hyva-themes/magento2-default-theme
+fi
+
+if [ "${INSTALL_MAGENTO}" = "NO" ] && [ -f ".gitpod/magento-db.sql.zip" ]; then
+  ddev import-db --src=.gitpod/magento-db.sql.zip
+else
+  echo "No database was imported. No .gitpod/magento-db.sql.zip was provided."
+fi
+if [ -f ".gitpod/files.tgz" ]; then
+  ddev import-files --src=.gitpod/files.tgz
+else
+  echo "No files.tgz was provided in .gitpod"
+fi
+
+ddev magento deploy:mode:set developer
+ddev magento setup:upgrade
+ddev magento config:set web/cookie/cookie_path "/"
+ddev magento config:set web/cookie/cookie_domain ".gitpod.io"
+ddev magento setup:store-config:set --base-url="${url}"
+ddev magento setup:store-config:set --base-url-secure="${url}"
+ddev magento setup:config:set --session-save=redis --session-save-redis-host=redis --session-save-redis-log-level=3 --session-save-redis-db=0 --session-save-redis-port=6379 -n;
+ddev magento setup:config:set --cache-backend=redis --cache-backend-redis-server=redis --cache-backend-redis-db=1 -n;
+ddev magento setup:config:set --page-cache=redis --page-cache-redis-server=redis --page-cache-redis-db=2 -n;
+ddev magento module:disable Magento_Csp Magento_TwoFactorAuth
+
+if [ "${MAGENTO_EDITION}" = "enterprise" ]; then
+    ddev exec magerun2 config:store:set btob/website_configuration/company_active "1"
+    ddev exec magerun2 config:store:set btob/website_configuration/sharedcatalog_active "0"
+    ddev exec magerun2 config:store:set btob/website_configuration/negotiablequote_active "0"
+    ddev exec magerun2 config:store:set btob/website_configuration/quickorder_active "0"
+    ddev exec magerun2 config:store:set btob/website_configuration/requisition_list_active "0"
+    ddev exec magerun2 config:store:set btob/default_b2b_payment_methods/available_payment_methods "braintree_ach_direct_debit,braintree_applepay,banktransfer,cashondelivery,checkmo,braintree,payflow_advanced,payflow_link,payflowpro,braintree_googlepay,braintree_local_payment,free,braintree_paypal,paypal_billing_agreement,payflow_express_bml,paypal_express_bml,paypal_express,payflow_express,hosted_pro,companycredit,purchaseorder,braintree_paypal_vault,braintree_cc_vault,payflowpro_cc_vault,braintree_venmo"
+    ddev exec magerun2 config:store:set btob/default_b2b_shipping_methods/applicable_shipping_methods "0"
+    ddev exec magerun2 config:store:set btob/website_configuration/purchaseorder_enabled "1"
+    ddev exec magerun2 config:store:set catalog/magento_catalogpermissions/enabled "1"
+    ddev exec magerun2 config:store:set catalog/magento_catalogpermissions/grant_catalog_category_view "1"
+    ddev exec magerun2 config:store:set catalog/magento_catalogpermissions/grant_catalog_product_price "1"
+    ddev exec magerun2 config:store:set catalog/magento_catalogpermissions/grant_checkout_items "1"
+    ddev exec magerun2 config:store:set sales/product_sku/my_account_enable "1"
+fi &&
+
+if [ "${INSTALL_HYVA}" = "YES" ] && [ ! -z "${HYVA_COMPOSER_TOKEN}" ] && [ ! -z "${HYVA_COMPOSER_PROJECT}" ]; then
+    # TODO: find a more reliable way to check hyva/default theme ID
+    if ddev exec magerun2 dev:theme:list | grep -q "spectrum"; then
+        ddev exec magerun2 config:store:set design/theme/theme_id 6
+    else
+        ddev exec magerun2 config:store:set design/theme/theme_id 5
+    fi
+
+    ddev exec magerun2 config:store:set customer/captcha/enable 0
+fi
+ddev magento cache:flush
+ddev redis-cli flushall
+
+touch ${CODESPACES_REPO_ROOT}/.gitpod/db-installed.flag
+gp ports await 8080 && sleep 5 && gp preview $(gp url 8080)
---
+++ .devcontainer/start_repo.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -eu
+
+url=$(gp url | awk -F"//" {'print $2'}) && url+="/" &&
+url="https://8080-"$url"/"
+
+MAGENTO_EDITION="${MAGENTO_EDITION:=community}"
+MAGENTO_VERSION="${MAGENTO_VERSION:=2.4.5-p1}"
+
+cd ${CODESPACES_REPO_ROOT}
+# Temporarily use an empty config.yaml to get ddev to use defaults
+# so we can do composer install. If there's already one there,
+# this does no harm.
+mkdir -p .ddev && touch .ddev/config.yaml
+
+# If there's a composer.json, do `ddev composer install` (which auto-starts projct)
+if [ -f composer.json ]; then
+  ddev start
+  ddev composer install
+else
+  ddev composer config -g -a http-basic.repo.magento.com ${MAGENTO_COMPOSER_AUTH_USER} ${MAGENTO_COMPOSER_AUTH_PASS}
+  ddev composer create --no-interaction --no-progress --repository-url=https://repo.magento.com/ magento/project-${MAGENTO_EDITION}-edition=${MAGENTO_VERSION}
+  git reset && git checkout . && git checkout -- .gitignore;
+fi
+
+sed -i "s/AUTH_USER/${MAGENTO_COMPOSER_AUTH_USER}/g" ${CODESPACES_REPO_ROOT}/auth.json &&
+sed -i "s/AUTH_PASS/${MAGENTO_COMPOSER_AUTH_PASS}/g" ${CODESPACES_REPO_ROOT}/auth.json &&
+
+if [ ! -f bin/magerun2 ]; then
+  ddev exec curl -L https://files.magerun.net/n98-magerun2.phar --output bin/magerun2
+  ddev exec chmod +x bin/magerun2
+fi
+
+if [ -f ".gitpod/db-installed.flag" ]; then
+  #Update the base url in Magento if the db is installed. Base url can change when stopping and starting a gitpod workspace.
+  ddev magento setup:store-config:set --base-url="${url}"
+  ddev magento setup:store-config:set --base-url-secure="${url}"
+fi
+
+ddev get drud/ddev-redis
+ddev get drud/ddev-elasticsearch
+ddev stop -a
+ddev start -y
